5fe91b1d60727df75bae85425a7efba3abae217c
comeheral <comeheral@gmail.com>
2023-10-17 19:32:32 +0200

    text component : support rich text

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,6 +2,7 @@
   Props :
   - size : sm, md, lg
   - color : dark, white
+  - richText : true, false
 */}
 
 let textSizes = {
@@ -10,10 +11,12 @@ let textSizes = {
   lg: "text-[20px] md:text-[24px]"
 }
 
-function Text({ className = "", children, size, color = "dark" }){
-  return(
-    <p className={`font-text font-light leading-normal text-${color} ${textSizes[size]} ${className}`}>{children}</p>
-  )
+function Text({ className = "", children, size, color = "dark", richText = false }){
+  if(richText){
+    return <div className={`font-text font-light leading-normal text-${color} ${textSizes[size]} ${className}`} dangerouslySetInnerHTML={{__html: children}}></div>
+  }else{
+    return <p className={`font-text font-light leading-normal text-${color} ${textSizes[size]} ${className}`}>{children}</p>
+  }
 }
 
 export default Text;
