8ed0fb34cfeb31ba6aea3857a74f5735451b7aec
comeheral <comeheral@gmail.com>
2023-10-17 19:32:58 +0200

    section hero : content style

--- src/components/SectionHero.jsx
+++ src/components/SectionHero.jsx
@@ -4,12 +4,14 @@ import Button from '@/components/Button';
 
 function SectionHero({ title, text, buttons = [] }){
   return (
-    <section className="page-width">
-      <Heading as="h1" size="lg">{title}</Heading>
-      <Text size="lg">{text}</Text>
-      {
-        buttons.map((button, index) => <Button key={`button-${index}`} link={button.item.link} variant={button.item.variant} size={button.item.size}>{button.item.label}</Button>)
-      }
+    <section className="px-[32px] max-w-[500px] md:max-w-[1192px] mx-auto h-screen md:flex md:items-center">
+      <div className="md:w-1/2">
+        <Heading className="mb-4 md:mb-8" as="h1" size="lg">{title}</Heading>
+        <Text className="mb-8 md:mb-10 [&>p]:mb-5" size="lg" richText={true}>{text}</Text>
+        {
+          buttons.map((button, index) => <Button className="mr-5 last:mr-0" key={`button-${index}`} link={button.item.link} variant={button.item.variant} size={button.item.size}>{button.item.label}</Button>)
+        }
+      </div>
     </section>
   )
 }
